8b215e443cb85ddd11de13bbc136f836e9e97355
Kshitij Gupta <kshitijgm@gmail.com>
2018-10-07 14:00:00 +0100

    Overlays: Switch to Lawnchair [2/3]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- overlays.mk
+++ overlays.mk
@@ -4,6 +4,11 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
 
+# Lawnchair
+ifeq ($(LAWNCHAIR_OPTOUT),)
+PRODUCT_PACKAGE_OVERLAYS += vendor/overlay/lawnchair
+endif
+
 # Extra packages
 PRODUCT_PACKAGES += \
     AOSiPOverlayStub \
