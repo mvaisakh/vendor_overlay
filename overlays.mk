d2109fa1631c76e1b76b850ba0f3cda1b3b10cc8
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-02-05 19:27:00 +0100

    Revert "Overlays: Switch to Lawnchair [2/3]"
    This reverts commit 8b215e443cb85ddd11de13bbc136f836e9e97355.
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- overlays.mk
+++ overlays.mk
@@ -4,11 +4,6 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
 
-# Lawnchair
-ifeq ($(LAWNCHAIR_OPTOUT),)
-PRODUCT_PACKAGE_OVERLAYS += vendor/overlay/lawnchair
-endif
-
 # Extra packages
 PRODUCT_PACKAGES += \
     AOSiPOverlayStub \
